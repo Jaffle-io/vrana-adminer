Jaffle-io/vrana-adminer

commit 778bba525445dc07ed7a0c84cf595f1a37420ceb

Allow using  in ->login() (bug #381)

adminer/include/auth.inc.php
@@ -137,12 +137,12 @@ if (isset($_GET["username"])) {
 	$connection = connect();
 }
 
+$driver = new Min_Driver($connection);
+
 if (!is_object($connection) || !$adminer->login($_GET["username"], get_password())) {
 	auth_error();
 	exit;
 }
-
-$driver = new Min_Driver($connection);
 
 if ($auth && $_POST["token"]) {
 	$_POST["token"] = $token; // reset token after explicit login

commit 70574c23c0b9ffb3d014e1b80a232b361ae21c24

Fix single driver non-MySQL compiled versions (regression from 778bba52)

adminer/include/auth.inc.php
@@ -171,9 +171,8 @@ if (isset($_GET["username"])) {
 	}
 	check_invalid_login();
 	$connection = connect();
+	$driver = new Min_Driver($connection);
 }
-
-$driver = new Min_Driver($connection);
 
 if (!is_object($connection) || ($login = $adminer->login($_GET["username"], get_password())) !== true) {
 	auth_error((is_string($connection) ? h($connection) : (is_string($login) ? $login : lang('Invalid credentials.'))));