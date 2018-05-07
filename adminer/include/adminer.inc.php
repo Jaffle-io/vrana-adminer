f9d11afded02dfc4124377bc8c05d5c32f44fce1
Jakub Vrana <jakub@vrana.cz>
2018-05-07 12:21:31 +0200

    Improve error message

--- adminer/include/adminer.inc.php
+++ adminer/include/adminer.inc.php
@@ -145,8 +145,8 @@ class Adminer {
 	* @return mixed true for success, string for error message, false for unknown error
 	*/
 	function login($login, $password) {
-		if ($\1REDACTED_SECRET "") { //! password is optional in Elastic and Mongo
-			return lang('<a href="https://www.adminer.org/en/extension/"%s>Implement</a> %s method to use password-less database.', target_blank(), '<code>login()</code>');
+		if ($\1REDACTED_SECRET "") {
+			return lang('Adminer does not support accessing a database without a password, <a href="https://www.adminer.org/en/password/"%s>more information</a>.', target_blank());
 		}
 		return true;
 	}
